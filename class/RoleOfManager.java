d855ed2a145eec59297816c5ee0ed0b932b2c2c1
khanglag <94234570+khanglag@users.noreply.github.com>
2022-11-30 19:25:38 +0700

    Create RoleOfManager.java

---
+++ class/RoleOfManager.java
@@ -0,0 +1,18 @@
+public interface RoleOfManager {
+    // public abstract void option();
+
+    public abstract void inputList();
+
+    public abstract void outputList();
+
+    public abstract void add();
+
+    public abstract void remove(String idEmp);
+
+    public abstract void edit(String idEmp);
+
+    public abstract void find(String idEmp);
+    
+    public abstract void statistics();
+
+}
