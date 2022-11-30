f55cd20c0b0f850acff81b08b11c64110ba35a2e
khanglag <94234570+khanglag@users.noreply.github.com>
2022-11-30 19:21:30 +0700

    Create App.java

---
+++ class/App.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class App {
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void init() {
+        FileUtil.getData();
+    }
+
+    public static void start() {
+        Handle.clearScreen();
+        Handle.login();
+    }
+
+    public static void end() {
+        FileUtil.setData();
+    }
+
+    public static void main(String[] args) {
+        init();
+        start();
+        end();
+    }
+}
