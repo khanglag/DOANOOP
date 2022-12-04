3d06b91affda5b6ae96ddca97775d6ab29cc367b
khanglag <94234570+khanglag@users.noreply.github.com>
2022-12-04 22:37:56 +0700

    Update Salary.java

--- class/Salary.java
+++ class/Salary.java
@@ -3,43 +3,62 @@ import java.util.Scanner;
 
 public class Salary implements InOut {
     static Scanner scanner = new Scanner(System.in);
-    private int indexSalary;
+    private double indexSalary;
     private int someHolidays;
     private double overtime;
     private long totalSalary;
+    protected long baseSalary = 200000;
 
     public Salary() {
     }
 
-    public Salary(int indexSalary,double overtime, int someHolidays, long totalSalary) {
+    public Salary(double indexSalary,double overtime, int someHolidays, long totalSalary) {
         this.indexSalary = indexSalary;
         this.overtime = overtime;
         this.someHolidays = someHolidays;
         this.totalSalary = totalSalary;
     }
 
-    public int getIndexSalary() {
+    public double getIndexSalary() {
         return indexSalary;
     }
 
-    public void setIndexSalary(int indexSalary) {
+   
+    public void setIndexSalary(double indexSalary) {
+        
         this.indexSalary = indexSalary;
     }
 
+    public void setIndexSalary() {
+        System.out.print("==> Enter a number of index salary: ");
+        indexSalary = scanner.nextDouble();
+        this.indexSalary = indexSalary;
+    }
     public double getOvertime() {
         return overtime;
     }
 
     public void setOvertime(double overtime) {
-        System.out.print("==> Enter a number of overtime: ");
+       
         this.overtime = overtime;
     }
 
+    public void setOvertime() {
+        System.out.print("==> Enter a number of overtime: ");
+        overtime = scanner.nextDouble();
+        this.overtime = overtime;
+    }
+
+    
     public int getSomeHolidays() {
         return someHolidays;
     }
 
     public void setSomeHolidays(int someHolidays) {
+        this.someHolidays = someHolidays;
+    }
+    
+    public void setSomeHolidays() {
         System.out.print("==> Enter a number of day-off: ");
         someHolidays = scanner.nextInt();
         this.someHolidays = someHolidays;
@@ -55,19 +74,22 @@ public class Salary implements InOut {
 
     @Override
     public void input() {
-        setSomeHolidays(someHolidays);
+        setIndexSalary();
+        setOvertime();
+        setSomeHolidays();
+        
     }
 
     @Override
     public void output() {
-        DecimalFormat formatter = new DecimalFormat("###,###,###");
-        System.out.printf("|  %-20s|  %-20s|  %-20s|", indexSalary, someHolidays,
+        DecimalFormat formatter = new DecimalFormat("###,###,###,###,###");
+        System.out.printf("|  %-20s|  %-20s|  %-20s| %-20s| %-20s|",baseSalary, indexSalary, overtime, someHolidays,
                 formatter.format(totalSalary) + " VND");
         System.out.println();
     }
 
-    public Long payRoll(Long baseSalary, int indexSalary) {
-        totalSalary = (long) baseSalary * indexSalary * 30 - someHolidays * baseSalary + (long) (overtime*25000);
+    public Long payRoll() {
+        totalSalary = (long) (baseSalary * indexSalary * 26 - someHolidays * baseSalary + (long) (overtime*25000));
         return totalSalary;
     }
 
